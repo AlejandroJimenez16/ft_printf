2cb9542112d10e65d23b9ff5dc0c7ab6c7e1be1d
Alejandro Jimenez Garcia <alejandj@c1r13s4.42madrid.com>
2025-02-18 15:57:09 +0100

    Perfect

--- ft_putnbr_hex.c
+++ ft_putnbr_hex.c
@@ -6,15 +6,15 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 11:30:25 by alejandj          #+#    #+#             */
-/*   Updated: 2025/02/18 11:36:19 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/02/18 13:02:31 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_hex(unsigned int nbr, char* base)
+int	ft_putnbr_hex(unsigned int nbr, char *base)
 {
-	int		size;
+	int	size;
 
 	size = 0;
 	if (nbr >= 16)
--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 12:25:54 by alejandj          #+#    #+#             */
-/*   Updated: 2025/02/13 13:40:29 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/02/18 13:03:07 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_putstr(char *s)
 	int	size;
 
 	if (!s)
-		return(ft_putstr("(null)"));
+		return (ft_putstr("(null)"));
 	i = 0;
 	size = 0;
 	while (s[i] != '\0')
