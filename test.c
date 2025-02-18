569345f0f7c52c5d507011dd1e7a37be46c07041
Alejandro Jimenez Garcia <alejandj@c1r13s4.42madrid.com>
2025-02-18 12:58:17 +0100

    Tester

--- test.c
+++ test.c
@@ -6,14 +6,16 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 13:24:09 by alejandj          #+#    #+#             */
-/*   Updated: 2025/02/18 12:15:21 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/02/18 12:57:22 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int main() {
-    
+#include <stdio.h>
+
+int main(void) 
+{    
     char c = 'A';
     char *s = "Hello, world!";
     void *p = (void *)s;
@@ -25,107 +27,95 @@ int main() {
     char *nula = NULL;
     
     // Casos básicos
-    printf("1. Caracter (original): %c", c);
-    ft_printf("1. Caracter (copia): %c", c);
-    printf("\n");
-    
-    printf("2. Cadena (original): %s", s);
-    ft_printf("2. Cadena (copia): %s", s);
-    printf("\n");
-    
-    printf("3. Puntero (original): %p", p);
-    ft_printf("3. Puntero (copia): %p", p);
-    printf("\n");
-    
-    printf("4. Entero (d) (original): %d", d);
-    ft_printf("4. Entero (d) (copia): %d", d);
-    printf("\n");
-    
-    printf("5. Entero (i) (original): %i", i);
-    ft_printf("5. Entero (i) (copia): %i", i);
-    printf("\n");
-    
-    printf("6. Unsigned (original): %u", u);
-    ft_printf("6. Unsigned (copia): %u", u);
-    printf("\n");
-    
-    printf("7. Hexadecimal (x) (original): %x", x);
-    ft_printf("7. Hexadecimal (x) (copia): %x", x);
+    printf("1. Caracter (original): %c\n", c);
+    ft_printf("1. Caracter (copia): %c\n", c);
     ft_printf("\n");
     
-    printf("8. Hexadecimal (X) (original): %X", X);
-    ft_printf("8. Hexadecimal (X) (copia): %X", X);
+    printf("2. Cadena (original): %s\n", s);
+    ft_printf("2. Cadena (copia): %s\n", s);
     ft_printf("\n");
     
-    printf("9. Porcentaje (original): %%");
-    ft_printf("9. Porcentaje (copia): %%");
+    printf("3. Puntero (original): %p\n", p);
+    ft_printf("3. Puntero (copia): %p\n", p);
+    ft_printf("\n");
+    
+    printf("4. Entero (d) (original): %d\n", d);
+    ft_printf("4. Entero (d) (copia): %d\n", d);
+    ft_printf("\n");
+    
+    printf("5. Entero (i) (original): %i\n", i);
+    ft_printf("5. Entero (i) (copia): %i\n", i);
+    ft_printf("\n");
+    
+    printf("6. Unsigned (original): %u\n", u);
+    ft_printf("6. Unsigned (copia): %u\n", u);
+    ft_printf("\n");
+    
+    printf("7. Hexadecimal (x) (original): %x\n", x);
+    ft_printf("7. Hexadecimal (x) (copia): %x\n", x);
+    ft_printf("\n");
+    
+    printf("8. Hexadecimal (X) (original): %X\n", X);
+    ft_printf("8. Hexadecimal (X) (copia): %X\n", X);
+    ft_printf("\n");
+    
+    printf("9. Porcentaje (original): %%\n");
+    ft_printf("9. Porcentaje (copia): %%\n");
     ft_printf("\n");
 
     // Casos especiales
-    printf("10. Carácter nulo (original): %c", '\0');
-    ft_printf("10. Carácter nulo (copia): %c", '\0');
+    printf("10. Carácter nulo (original): %c\n", '\0');
+    ft_printf("10. Carácter nulo (copia): %c\n", '\0');
     ft_printf("\n");
     
-    printf("11. Cadena nula (original): %s", nula);
-    ft_printf("11. Cadena nula (copia): %s", nula);
+    printf("11. Cadena nula (original): %s\n", nula);
+    ft_printf("11. Cadena nula (copia): %s\n", nula);
     ft_printf("\n");
     
-    printf("12. Puntero nulo (original): %p", NULL);
-    ft_printf("12. Puntero nulo (copia): %p", NULL);
+    printf("12. Puntero nulo (original): %p\n", NULL);
+    ft_printf("12. Puntero nulo (copia): %p\n", NULL);
     ft_printf("\n");
     
-    printf("13. INT_MIN (original): %d", INT_MIN);
-    ft_printf("13. INT_MIN (copia): %d", INT_MIN);
+    printf("13. INT_MIN (original): %d\n", INT_MIN);
+    ft_printf("13. INT_MIN (copia): %d\n", INT_MIN);
     ft_printf("\n");
     
-    printf("14. INT_MAX (original): %d", INT_MAX);
-    ft_printf("14. INT_MAX (copia): %d", INT_MAX);
+    printf("14. INT_MAX (original): %d\n", INT_MAX);
+    ft_printf("14. INT_MAX (copia): %d\n", INT_MAX);
     ft_printf("\n");
     
-    printf("15. UINT_MAX (original): %u", UINT_MAX);
-    ft_printf("15. UINT_MAX (copia): %u", UINT_MAX);
+    printf("15. UINT_MAX (original): %u\n", UINT_MAX);
+    ft_printf("15. UINT_MAX (copia): %u\n", UINT_MAX);
     ft_printf("\n");
     
-    printf("16. Hexadecimal negativo (original): %x", -1);
-    ft_printf("16. Hexadecimal negativo (copia): %x", -1);
+    printf("16. Hexadecimal negativo (original): %x\n", -1);
+    ft_printf("16. Hexadecimal negativo (copia): %x\n", -1);
     ft_printf("\n");
     
-    printf("17. Entero 0 (original): %d", 0);
-    ft_printf("17. Entero 0 (copia): %d", 0);
-    ft_printf("\n");
-
-    // Flags y formatos avanzados
-    printf("18. Flag más (original): %+d", 42);
-    ft_printf("18. Flag más (copia): %+d", 42);
-    ft_printf("\n");
-    
-    printf("19. Flag espacio (original): % d", 42);
-    ft_printf("19. Flag espacio (copia): % d", 42);
-    ft_printf("\n");
-    
-    printf("20. Flag cero con ancho (original): %010d", 42);
-    ft_printf("20. Flag cero con ancho (copia): %010d", 42);
-    ft_printf("\n");
-    
-    printf("21. Ancho y precisión (original): %10.5d", 42);
-    ft_printf("21. Ancho y precisión (copia): %10.5d", 42);
-    ft_printf("\n");
-    
-    printf("22. Truncado de string (original): %.3s", "abcdef");
-    ft_printf("22. Truncado de string (copia): %.3s", "abcdef");
+    printf("17. Entero 0 (original): %d\n", 0);
+    ft_printf("17. Entero 0 (copia): %d\n", 0);
     ft_printf("\n");
 
     // Pruebas de múltiples argumentos
-    printf("23. Múltiples valores (original): %d %s %x", 42, "test", 255);
-    ft_printf("23. Múltiples valores (copia): %d %s %x", 42, "test", 255);
+    printf("18. Múltiples valores (original): %d %s %x\n", 42, "test", 255);
+    ft_printf("18. Múltiples valores (copia): %d %s %x\n", 42, "test", 255);
     ft_printf("\n");
     
-    printf("24. Saltos de línea (original): %s", "Hola\nMundo");
-    ft_printf("24. Saltos de línea (copia): %s", "Hola\nMundo");
+    printf("19. Saltos de línea (original): %s\n", "Hola\nMundo");
+    ft_printf("19. Saltos de línea (copia): %s\n", "Hola\nMundo");
     ft_printf("\n");
-    
-    printf("25. Tabulación (original): %s", "Hola\tMundo");
-    ft_printf("25. Tabulación (copia): %s", "Hola\tMundo");
+
+    printf("20. Hexadecimal con INT_MIN (original): %x\n", INT_MIN);
+    ft_printf("20. Hexadecimal con INT_MIN (copia): %x\n", INT_MIN);
+    ft_printf("\n");
+
+    printf("21. Hexadecimal con INT_MAX (original): %x\n", INT_MAX);
+    ft_printf("21. Hexadecimal con INT_MAX (copia): %x\n", INT_MAX);
+    ft_printf("\n");
+
+    printf("22. Triple porcentaje (original): %%%d%%\n", 42);
+    ft_printf("22. Triple porcentaje (copia): %%%d%%\n", 42);
+    ft_printf("\n");
     
     return 0;
 }
